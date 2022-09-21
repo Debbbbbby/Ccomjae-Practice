3b40876fa75341667b1678f738e391abdabd12fe
Debby <debby_@kakao.com>
2022-09-21 23:11:16 +0900

    DepartmentDAO : setting DAO (1)

---
+++ Chapter06-HR/Chapter06-HR/Chapter06-HR-Bridging-Header.h
@@ -0,0 +1,5 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+// 표준 라이브러리가 아니라면 < 대신 "로 표시
+
+#import "FMDB.h"
---
+++ Chapter06-HR/Chapter06-HR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by Debby on 2022/09/21.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
