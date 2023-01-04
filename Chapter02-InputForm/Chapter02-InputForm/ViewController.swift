0a00eda872b865feb37aec5922894f73a480233b
Debby <debby_@kakao.com>
2023-01-04 19:09:54 +0900

    ✨ 2.3.4 커스텀 코드 - 입력폼

---
+++ Chapter02-InputForm/Chapter02-InputForm/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Chapter02-InputForm
+//
+//  Created by Doyeon on 2023/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var paramEmail: UITextField!
+    var paramUpdate: UISwitch!
+    var paramInterval: UIStepper!
+    
+    var txtUpdate: UILabel! // 스위치 컨트롤 값 표현
+    var txtInterval: UILabel! // 스테퍼 컨트롤 값 표현
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "설정"
+        
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        
+        lblEmail.font = UIFont.systemFont(ofSize: 22)
+        //        lblEmail.font = UIFont.boldSystemFont(ofSize: 22)
+        //        lblEmail.font = UIFont(name: "Chalkboard SE", size: 22)
+        
+        self.view.addSubview(lblEmail)
+        
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        
+        lblUpdate.font = UIFont.systemFont(ofSize: 22)
+        
+        self.view.addSubview(lblUpdate)
+        
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        
+        lblInterval.font = UIFont.systemFont(ofSize: 22)
+        
+        self.view.addSubview(lblInterval)
+        
+        self.paramEmail = UITextField()
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 20)
+        self.paramEmail.borderStyle = .roundedRect
+        self.paramEmail.autocapitalizationType = .none // 자동 대문자 변환 기능 해제
+        
+        self.view.addSubview(self.paramEmail)
+        
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        self.paramInterval = UIStepper()
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0
+        self.paramInterval.maximumValue = 100
+        self.paramInterval.stepValue = 1
+        self.paramInterval.value = 0
+        
+        self.view.addSubview(self.paramInterval)
+        
+        self.txtUpdate = UILabel()
+        
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 18)
+        self.txtUpdate.textColor = UIColor.red
+        self.txtUpdate.text = "갱신함"
+        
+        self.view.addSubview(self.txtUpdate)
+        
+        self.txtInterval = UILabel()
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.font = UIFont.systemFont(ofSize: 18)
+        self.txtInterval.textColor = UIColor.red
+        self.txtInterval.text = "0분마다"
+        
+        self.view.addSubview(self.txtInterval)
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue), for: .valueChanged)
+    }
+    
+    // 스위치와 상호작용 액션
+    @objc func presentUpdateValue(_ sender: UISwitch) {
+        self.txtUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
+    
+    // 스테퍼와 상호작용 액션
+    @objc func presentIntervalValue(_ sender: UIStepper) {
+        self.txtInterval.text = ("\(Int(sender.value))분마다") // sender.value가 실수형이라서 Int형으로 형변환
+    }
+    
+    // 16진수 색상값을 UIColor 객체로 변환하는 함수
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+}
+
