bc001bec506b55336bad4f84e6a2f9d298bd6fa4
doyeonjeong <debby_@kakao.com>
2023-02-01 20:51:31 +0900

    ♻ 3.2.1 UI커스텀 - 기본적인 내비게이션바 타이틀

--- Chpater03-NavigationBar/Chpater03-NavigationBar/ViewController.swift
+++ Chpater03-NavigationBar/Chpater03-NavigationBar/ViewController.swift
@@ -17,6 +17,21 @@ class ViewController: UIViewController {
 
     // 내비게이션 바 타이틀 구현
     func initTitle() {
+        // 1. 내비게이션 타이틀용 레이블 객체
+        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
         
+        // 2. 속성 설정
+        nTitle.numberOfLines = 2 // 두 줄까지 표시되도록 설정
+        nTitle.textAlignment = .center
+        nTitle.textColor = .white
+        nTitle.font = UIFont.systemFont(ofSize: 18)
+        nTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일)"
+        
+        // 3. 내비게이션 타이틀에 입력
+        self.navigationItem.titleView = nTitle
+        
+        // 배경 색상 설정
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.backgroundColor = color
     }
 }
