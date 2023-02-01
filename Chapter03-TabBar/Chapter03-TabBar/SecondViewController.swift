084c88ce6671458f56cd67caab1eb3e5941d5068
doyeonjeong <debby_@kakao.com>
2023-02-01 15:18:27 +0900

    ✨ 3.3.1 UI커스텀 - 탭바

--- Chapter03-TabBar/Chapter03-TabBar/SecondViewController.swift
+++ Chapter03-TabBar/Chapter03-TabBar/SecondViewController.swift
@@ -11,19 +11,14 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        title.text = "두번째 탭"
+        title.textColor = .red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 18)
+        title.sizeToFit()
+        title.center.x = self.view.frame.width / 2
+        self.view.addSubview(title)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
+++ Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 1) title 레이블 생성
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        // 2) title 레이블 속성 설정
+        title.text = "첫번째 탭"
+        title.textColor = .red
+        title.textAlignment = .center
+        title.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        // 3) 콘텐츠 내용에 맞게 레이블 크기 변경 -> 사용하지 않을 경우 엘립시스 발생
+        title.sizeToFit()
+        
+        // 4) X축의 중앙에 오도록 설정
+        /// [주의]
+        /// 레이블 속성 설정한 후 사용해야 콘텐츠 길이가 길어져도 center에 정렬됨
+        /// center 속성이 설정된 후에 객체의 크기가 변하면 한쪽으로 쏠리는 것 처럼 보일 수 있음
+        title.center.x = self.view.frame.width / 2 // frame.width는 get-only 설정, 값을 대입하려면 frame.size.width 사용해야함
+        
+        // 5) 슈퍼 뷰에 추가
+        self.view.addSubview(title)
     }
-
-
 }
-
