57e391b2a6b3006aa19c368b79494ad8ac2deca2
doyeonjeong <debby_@kakao.com>
2023-02-01 19:03:43 +0900

    ✨ 3.1.6 UI커스텀 - 탭바 숨기기

--- Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
+++ Chapter03-TabBar/Chapter03-TabBar/ViewController.swift
@@ -38,4 +38,16 @@ class ViewController: UIViewController {
 //        self.tabBarItem.image = UIImage(named: "calendar")
 //        self.tabBarItem.title = "Calendar"
     }
+    
+    // MARK: touchesEnded - 터치가 끝났을 때 호출되는 메서드
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let tabBar = self.tabBarController?.tabBar
+//        tabBar?.isHidden = (tabBar?.isHidden == true) ? false : true
+        
+        UIView.animate(withDuration: TimeInterval(1.5)) {
+            // alpha 값이 0이면 1로, 1이면 0으로 바꾸어 준다.
+            // 호출될 때마다 점점 투명해졌다가 점점 진해지도록 설정
+            tabBar?.alpha = ( tabBar?.alpha == 0 ? 1 : 0 )
+        }
+    }
 }
