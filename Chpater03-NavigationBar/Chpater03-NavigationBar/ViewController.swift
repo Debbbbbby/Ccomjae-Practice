e1d42daf38f3bdc6b0e2559f517af46ce6a9cd4a
doyeonjeong <debby_@kakao.com>
2023-02-01 19:18:29 +0900

    🎉 3.2 UI커스텀 - 내비게이션바

---
+++ Chpater03-NavigationBar/Chpater03-NavigationBar/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Chpater03-NavigationBar
+//
+//  Created by Doyeon on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 내비게이션 타이틀 초기화
+        self.initTitle()
+    }
+
+    // 내비게이션 바 타이틀 구현
+    func initTitle() {
+        
+    }
+}
