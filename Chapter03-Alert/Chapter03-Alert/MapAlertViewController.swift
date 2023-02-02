67f58758177c36bba84045899ee6106c0e25e791
doyeonjeong <debby_@kakao.com>
2023-02-02 18:20:34 +0900

    ♻ 3.3.5 UI커스텀 - 알림창 / MapAlertViewController 리팩토링

--- Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/MapAlertViewController.swift
@@ -37,35 +37,8 @@ class MapAlertViewController: UIViewController {
         alert.addAction(okAction)
         
         // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성하고, 알림창에 등록한다.
-        let contentVC = UIViewController()
-        
-        // 뷰 컨트롤러에 맵킷 뷰를 추가한다.
-        let mapkitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        contentVC.view = mapkitView
-        // --> 여기까지만 설정하면 대한민국이 통째로 보인다.
-        
-        contentVC.preferredContentSize.height = 200
-        
-        // 맵킷 추가 설정
-        // 1) 위치 정보(위도, 경도) 설정
-        let pos = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
-        
-        // 2) 지도에서 보여줄 넓이, 일종의 축척, 숫자가 작을수록 좁은 범위를 확대시켜서 보여준다.
-        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
-        
-        // 3) 지도 영역을 정의
-        let region = MKCoordinateRegion(center: pos, span: span)
-        
-        // 4) 지도 뷰에 표시
-        mapkitView.region = region
-        mapkitView.regionThatFits(region)
-        
-        // 5) 위치를 핀으로 표시
-        let point = MKPointAnnotation()
-        point.coordinate = pos
-        mapkitView.addAnnotation(point)
-        
-        
+        let contentVC = MapKitViewController()
+
         // 뷰 컨트롤러를 알림창의 콘텐츠 뷰 컨트롤러 속성에 등록한다.
         alert.setValue(contentVC, forKey: "contentViewController")
         
