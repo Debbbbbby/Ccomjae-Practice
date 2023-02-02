cfc7d4cbcfda10c02e73edf368c691b17571d1b2
doyeonjeong <debby_@kakao.com>
2023-02-02 17:28:20 +0900

    ✨ 3.3.3 UI커스텀 - 알림창 / 기본화면 구성

--- Chapter03-Alert/Chapter03-Alert/ViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ViewController.swift
@@ -11,7 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 기본 알림창 버튼 생성
+        let defaultAlertBtn = UIButton(type: .system)
+        defaultAlertBtn.frame = CGRect(x: 0, y: 100, width: 100, height: 30)
+        defaultAlertBtn.center.x = self.view.frame.width / 2
+        defaultAlertBtn.setTitle("기본 알림창", for: .normal)
+        defaultAlertBtn.addTarget(self, action: #selector(defaultAlert(_:)), for: .touchUpInside)
+        self.view.addSubview(defaultAlertBtn)
+    }
+    
+    @objc func defaultAlert(_ sender: Any) {
+        // 1) 알림창 정의
+        let alert = UIAlertController(title: "알림창",
+                                      message: "기본 메세지",
+                                      preferredStyle: .alert)
+        
+        // 2) 버튼 정의
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        // 3) 버튼을 알림창에 추가
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        
+        // 4) 알림창을 화면에 표시
+        self.present(alert, animated: false)
     }
 
 
