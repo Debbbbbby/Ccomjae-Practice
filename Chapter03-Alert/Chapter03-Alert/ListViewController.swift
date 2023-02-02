779fcff7e0577c502c17f61a08cb586b341edad6
doyeonjeong <debby_@kakao.com>
2023-02-02 19:07:51 +0900

    ♻ 3.3.8 ListViewController 기본 설정

--- Chapter03-Alert/Chapter03-Alert/ListViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ListViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.preferredContentSize.height = 220
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "\(indexPath.row) 번째 옵션입니다."
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        return cell
+    }
+    
 }
