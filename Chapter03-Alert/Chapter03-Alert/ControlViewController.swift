3cea8742cebca7fda101a2b484293e431472dfcc
doyeonjeong <debby_@kakao.com>
2023-02-02 18:45:33 +0900

    ♻ 3.3.6 Slider 설정

--- Chapter03-Alert/Chapter03-Alert/ControlViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ControlViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class ControlViewController: UIViewController {
 
+    let slider = UISlider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 슬라이더의 최소값 / 최대값
+        self.slider.minimumValue = 0
+        self.slider.maximumValue = 100
+        
+        // 슬라이더의 영역과 크기를 정의하고 루트 뷰에 추가한다.
+        self.slider.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
+        self.view.addSubview(self.slider)
+        
+        self.preferredContentSize = CGSize(width: self.slider.frame.width, height: self.slider.frame.height + 10)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
