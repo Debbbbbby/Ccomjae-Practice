4a2e8b8c26015d9bd43264f8867d3793d87b217a
doyeonjeong <debby_@kakao.com>
2023-02-03 15:19:57 +0900

    ✨ 3.7.1  CSStepper 기본 설정

---
+++ Chapter03-CSStepper/Chapter03-CSStepper/CSStepper.swift
@@ -0,0 +1,22 @@
+//
+//  CSStepper.swift
+//  Chapter03-CSStepper
+//
+//  Created by Doyeon on 2023/02/03.
+//
+
+import UIKit
+
+class CSStepper: UIView {
+
+    // 스토리보드에서 호출할 초기화 메서드
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // 프로그래밍 방식으로 호출할 초기화 메서드
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+}
