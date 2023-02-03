70480831b369c04d47b594a1727b64b4031b7ce6
doyeonjeong <debby_@kakao.com>
2023-02-03 12:46:12 +0900

    ✨ 3.5.3 커스텀 타입을 사용한 버튼 생성

--- Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
+++ Chapter03-CSButton/Chapter03-CSButton/ViewController.swift
@@ -22,6 +22,15 @@ class ViewController: UIViewController {
         csBtn2.setTitle("CSButton2", for: .normal)
         csBtn2.center.x = self.view.center.x
         self.view.addSubview(csBtn2)
+        
+        // 인자값에 따라 다른 스타일로 결정되는 버튼 1
+        let rectBtn = CSButton(type: .rect)
+        rectBtn.frame = CGRect(x: 30, y: 200, width: 150, height: 30)
+        self.view.addSubview(rectBtn)
+        
+        let circleBtn = CSButton(type: .circle)
+        circleBtn.frame = CGRect(x: 200, y: 200, width: 200, height: 30)
+        self.view.addSubview(circleBtn)
     }
 }
 
