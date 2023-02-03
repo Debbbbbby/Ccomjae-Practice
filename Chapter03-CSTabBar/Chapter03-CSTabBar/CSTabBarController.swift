be59e07adba378d4af0e7c8a5940dd95a42b8c71
doyeonjeong <debby_@kakao.com>
2023-02-03 14:01:39 +0900

    ♻ 3.6.1 탭 바 영역의 view frame 지정하기

--- Chapter03-CSTabBar/Chapter03-CSTabBar/CSTabBarController.swift
+++ Chapter03-CSTabBar/Chapter03-CSTabBar/CSTabBarController.swift
@@ -19,5 +19,16 @@ class CSTabBarController: UITabBarController {
         
         // 기존 탭 바 숨김처리
         self.tabBar.isHidden = true
+        
+        // 새로운 탭 바 역할을 할 뷰를 위해 기준 좌표와 크기 정의
+        let width = self.view.frame.width
+        let height: CGFloat = 50
+        let x: CGFloat = 0
+        let y = self.view.frame.height - height
+        
+        // 정의된 값을 이용하여 새로운 뷰의 속성 설정
+        self.csView.frame = CGRect(x: x, y: y, width: width, height: height)
+        
+        self.view.addSubview(self.csView)
     }
 }
