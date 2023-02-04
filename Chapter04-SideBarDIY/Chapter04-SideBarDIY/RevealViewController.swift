2dc6d92451768fae21021b00e4074bc2e3d78d56
doyeonjeong <debby_@kakao.com>
2023-02-04 15:17:28 +0900

    ✨ getSideView() 구현

--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/RevealViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/RevealViewController.swift
@@ -42,5 +42,28 @@ class RevealViewController: UIViewController {
             vc.didMove(toParent: self)
         }
     }
+    
+    /// 사이드 바의 뷰 읽어오기
+    func getSideView() {
+        
+        guard self.sideVC == nil else { return } // side바가 없을 때 실행
+        
+        // 사이드 바 컨트롤러 객체를 읽어온다
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "sw_rear") else { return }
+        
+        // 다른 메서드에서도 참조할 수 있도록 sideVC 속성에 저장
+        self.sideVC = vc
+        
+        // 읽어온 사이드 바 컨트롤러 객체를 컨테이너 뷰 컨트롤러에 연결
+        self.addChild(vc)
+        self.view.addSubview(vc.view)
+        
+        // _프론트 컨트롤러에 부모 뷰 컨트롤러가 바뀌었음을 알려준다
+        vc.didMove(toParent: self)
+        
+        // _프론트 컨트롤러의 뷰를 제일 위로 올린다
+        self.view.bringSubviewToFront((self.contentVC?.view)!)
+    }
+    
 
 }
