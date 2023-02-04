90c93d24d4bb323bc7bfcc2b87a832d63018b2a3
doyeonjeong <debby_@kakao.com>
2023-02-04 15:42:38 +0900

    ✨ FrontView 사이드 바 열고 닫는 기능

--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class FrontViewController: UIViewController {
+    
+    // MARK: - Properties
+    /// 사이드 바 오픈 기능을 위임할 델리게이트
+    var delegate: RevealViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 사이드 바 오픈용 정의 버튼
+        let btnSideBar = UIBarButtonItem(image: UIImage(named: "sidemenu"),
+                                         style: UIBarButtonItem.Style.plain,
+                                         target: self,
+                                         action: #selector(moveSide(_:)) )
+        
+        // 버튼을 내비게이션 바의 왼쪽 영역에 추가
+        self.navigationItem.leftBarButtonItem = btnSideBar
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Action Method
+    /// 사용자의 액션에 따라 델리게이트 메서드를 호출
+    @objc func moveSide(_ sender: Any) {
+        if self.delegate?.isSideBarShowing == false {
+            self.delegate?.openSideBar(nil)
+        } else {
+            self.delegate?.closeSideBar(nil)
+        }
     }
-    */
 
 }
