ef254bc39c78ea48ea22f62f475b83031a8442c4
doyeonjeong <debby_@kakao.com>
2023-02-04 15:52:43 +0900

    ✨ 4.2.5 제스처 구현

--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/FrontViewController.swift
@@ -24,16 +24,32 @@ class FrontViewController: UIViewController {
         
         // 버튼을 내비게이션 바의 왼쪽 영역에 추가
         self.navigationItem.leftBarButtonItem = btnSideBar
+        
+        // 화면 끝에서 다른 쪽으로 패닝하는 제스처를 정의
+        let dragLeft = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(moveSide))
+        dragLeft.edges = UIRectEdge.left // 시작 모서리는 왼쪽
+        self.view.addGestureRecognizer(dragLeft) // 뷰에 제스처 객체를 등록
+        
+        // 화면을 스와이프하는 제스처 정의(사이드 메뉴 닫기)
+        let dragRight = UISwipeGestureRecognizer(target: self, action: #selector(moveSide))
+        dragRight.direction = .left
+        self.view.addGestureRecognizer(dragRight)
     }
     
     // MARK: - Action Method
     /// 사용자의 액션에 따라 델리게이트 메서드를 호출
     @objc func moveSide(_ sender: Any) {
-        if self.delegate?.isSideBarShowing == false {
+        
+        if sender is UIScreenEdgePanGestureRecognizer {
             self.delegate?.openSideBar(nil)
-        } else {
+        } else if sender is UISwipeGestureRecognizer {
             self.delegate?.closeSideBar(nil)
+        } else if sender is UIBarButtonItem {
+            if self.delegate?.isSideBarShowing == false {
+                self.delegate?.openSideBar(nil)
+            } else {
+                self.delegate?.closeSideBar(nil)
+            }
         }
     }
-
 }
