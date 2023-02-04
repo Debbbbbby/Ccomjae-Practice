a00297c813c0895dceb8abf4c613ae1397a63ef4
doyeonjeong <debby_@kakao.com>
2023-02-04 14:09:19 +0900

    ✨ 4.2.3 SideBar 기본 테이블 셀 정의

--- Chapter04-SideBarDIY/Chapter04-SideBarDIY/SideBarViewController.swift
+++ Chapter04-SideBarDIY/Chapter04-SideBarDIY/SideBarViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class SideBarViewController: UITableViewController {
-
+    // MARK: - Properties
+    /// 메뉴 제목 배열
+    let titles = [
+        "메뉴 01",
+        "메뉴 02",
+        "메뉴 03",
+        "메뉴 04",
+        "메뉴 05"
+    ]
+    
+    let icons = [
+        UIImage(named: "icon01"),
+        UIImage(named: "icon02"),
+        UIImage(named: "icon03"),
+        UIImage(named: "icon04"),
+        UIImage(named: "icon05")
+    ]
+    
+    // MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// 행의 개수 반환
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.titles.count
     }
-    */
+    
+    /// 각 행에 맞는 셀 객체 반환
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 1. 재사용 큐에 테이블 셀을 꺼내온다.
+        /// 재사용 큐에 등록할 식별자
+        let id = "menucell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: id)
+        
+        // 2. 재사용 큐에 menucell키로 등록된 테이블 뷰 셀이 없다면 새로 추가한다.
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: id)
+        }
+        
+        // 3. 타이틀과 이미지를 대입한다.
+        cell?.textLabel?.text = self.titles[indexPath.row]
+        cell?.imageView?.image = self.icons[indexPath.row]
+        
+        // 4. 폰트 설정
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        return cell!
+    }
 
 }
