e3e99915041e1500283230461aa5728aa51a14ce
doyeonjeong <debby_@kakao.com>
2023-02-07 20:59:30 +0900

    ✨ 5.3.3 새 계정 추가 기능 & 🔥 더미데이터 삭제

--- Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
+++ Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
@@ -9,17 +9,9 @@ import UIKit
 
 class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    // MARK: - Dummy
-    let accountList = [
-        "sqlpro@naver.com",
-        "debby_@kakao.com",
-        "asdf@gmail.com",
-        "qwer@naver.com",
-        "zxcv@kakao.com"
-    ]
-    
     // MARK: - Properties
     @IBOutlet weak var account: UITextField!
+    var accountlist = [String]()
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var gender: UISegmentedControl!
     @IBOutlet weak var married: UISwitch!
@@ -53,9 +45,22 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
                                         target: nil,
                                         action: nil)
+        
+        // 신규 계정 등록 버튼
+        let new = UIBarButtonItem()
+        new.title = "New"
+        new.target = self
+        new.action = #selector(newAccount)
 
         // 버튼을 툴 바에 추가
-        toolbar.setItems([flexSpace, done], animated: true)
+        toolbar.setItems([new, flexSpace, done], animated: true)
+        
+        // 기본 저장소 객체 불러오기
+        let plist = UserDefaults.standard
+        
+        self.name.text = plist.string(forKey: "name")
+        self.married.isOn = plist.bool(forKey: "married")
+        self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
     }
     
     // MARK: - @IBAction Methods
@@ -101,28 +106,25 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
 
     /// 지정된 컴포넌트가 가질 목록의 길이
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.accountList.count
+        return self.accountlist.count
     }
 
     /// 지정된 컴포넌트의 목록 각 행에 출력될 내용을 정의
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.accountList[row]
+        return self.accountlist[row]
     }
 
     /// 지정된 컴포넌트의 목록 각 행을 사용자가 선택했을 때 실행할 액션을 정의
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // 선택된 계정값을 텍스트 필드에 입력
-        let account = self.accountList[row]
+        let account = self.accountlist[row]
         self.account.text = account
     }
     
     // MARK: - UITableViewController Methods
     /// 사용자가 테이블 뷰 셀을 선택했을 때 호출되는 메서드
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if indexPath.row == 0 {
-            
-        } else if indexPath.row == 1 {
+        if indexPath.row == 1 {
             // 입력 가능한 알림창을 띄워 이름을 수정할 수 있도록 한다.
             let alert = UIAlertController(title: nil,
                                           message: "이름을 입력하세요.",
@@ -151,6 +153,32 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
     // MARK: - Action Methods
     @objc func pickerDone(_ sender: Any) {
         self.view.endEditing(true)
+    }
+    
+    @objc func newAccount(_ sender: Any) {
+        self.view.endEditing(true) // 일단 열려있는 입력용 뷰 부터 닫기
+        
+        let alert = UIAlertController(title: "새 계정을 입력하세요",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        alert.addTextField() {
+            $0.placeholder = "ex) abc@gmail.com"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { (_) in
+            if let account = alert.textFields?[0].text {
+                // 계정 목록 배열에 추가
+                self.accountlist.append(account)
+                // 계정 텍스트 필드에 표시
+                self.account.text = account
+                
+                // 컨트롤 값을 모두 초기화
+                self.name.text = ""
+                self.gender.selectedSegmentIndex = 0
+                self.married.isOn = false
+            }
+        })
+        self.present(alert, animated: false)
     }
 
 }
