804e34dfa513221342693f1e1b57efc4d2076fc3
doyeonjeong <debby_@kakao.com>
2023-02-07 21:31:33 +0900

    ♻️ 마스터 데이터 - 새 계정 정보 저장, 마지막 계정 데이터 불러오기

--- Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
+++ Chapter05-CustomPlist/Chapter05-CustomPlist/ListViewController.swift
@@ -58,6 +58,13 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
         // 기본 저장소 객체 불러오기
         let plist = UserDefaults.standard
         
+        let accountlist = plist.array(forKey: "accountlist") as? [String] ?? [String]()
+        self.accountlist = accountlist
+        
+        if let account = plist.string(forKey: "selectedAccount") {
+            self.account.text = account
+        }
+        
         self.name.text = plist.string(forKey: "name")
         self.married.isOn = plist.bool(forKey: "married")
         self.gender.selectedSegmentIndex = plist.integer(forKey: "gender")
@@ -119,6 +126,11 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
         // 선택된 계정값을 텍스트 필드에 입력
         let account = self.accountlist[row]
         self.account.text = account
+        
+        // 사용자가 계정을 생성하면 이 계정을 선택한 것으로 간주하고 저장
+        let plist = UserDefaults.standard
+        plist.set(account, forKey: "selectedAccount")
+        plist.synchronize()
     }
     
     // MARK: - UITableViewController Methods
@@ -176,6 +188,12 @@ class ListViewController: UITableViewController, UIPickerViewDelegate, UIPickerV
                 self.name.text = ""
                 self.gender.selectedSegmentIndex = 0
                 self.married.isOn = false
+                
+                // 계정 목록을 통째로 저장
+                let plist = UserDefaults.standard
+                plist.set(self.accountlist, forKey: "accountlist")
+                plist.set(account, forKey: "selectedAccount")
+                plist.synchronize()
             }
         })
         self.present(alert, animated: false)
