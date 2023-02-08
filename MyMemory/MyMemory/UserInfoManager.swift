b2ac01a3e83740cb135c278d500b25a7f46bf685
doyeonjeong <debby_@kakao.com>
2023-02-08 17:09:23 +0900

    ✨ 5.4.1 UserInfoManager 구현

--- MyMemory/MyMemory/UserInfoManager.swift
+++ MyMemory/MyMemory/UserInfoManager.swift
@@ -17,5 +17,89 @@ struct UserInfoKey {
 
 /// 계정 및 사용자 정보를 저장 관리하는 클래스
 class UserInfoManager {
+    // 연산 프로퍼티 loginId 정의
+    var loginId: Int {
+        get {
+            return UserDefaults.standard.integer(forKey: UserInfoKey.loginId)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.loginId)
+            ud.synchronize()
+        }
+    }
     
+    // 비로그인 상태일 때 nil값으로 반환하기 위해 옵셔널 타입으로 선언
+    var account: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.account)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.account)
+            ud.synchronize()
+        }
+    }
+    
+    var name: String? {
+        get {
+            return UserDefaults.standard.string(forKey: UserInfoKey.name)
+        }
+        set(v) {
+            let ud = UserDefaults.standard
+            ud.set(v, forKey: UserInfoKey.name)
+            ud.synchronize()
+        }
+    }
+    
+    var profile: UIImage? {
+        get {
+            let ud = UserDefaults.standard
+            if let _profile = ud.data(forKey: UserInfoKey.profile) {
+                return UIImage(data: _profile)
+            } else {
+                return UIImage(named: "account.jpg") // 이미지가 없다면 기본 이미지 반환
+            }
+        }
+        set(v) {
+            if v != nil {
+                let ud = UserDefaults.standard
+                ud.set(v!.pngData(), forKey: UserInfoKey.profile)
+                ud.synchronize()
+            }
+        }
+    }
+    
+    var isLogin: Bool {
+        // 로그인 아이디가 0이거나 계정이 비어있다면
+        if self.loginId == 0 || self.account == nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func login(account: String, passwd: String) -> Bool {
+        // TODO: 추후 서버 연동 코드로 대체될 예정
+        if account.isEqual("sqlpro@naver.com") && passwd.isEqual("1234") {
+            let ud = UserDefaults.standard
+            ud.set(100, forKey: UserInfoKey.loginId)
+            ud.set(account, forKey: UserInfoKey.account)
+            ud.set("도연 씨", forKey: UserInfoKey.name)
+            ud.synchronize()
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func logout() -> Bool {
+        let ud = UserDefaults.standard
+        ud.removeObject(forKey: UserInfoKey.loginId)
+        ud.removeObject(forKey: UserInfoKey.account)
+        ud.removeObject(forKey: UserInfoKey.name)
+        ud.removeObject(forKey: UserInfoKey.profile)
+        ud.synchronize()
+        return true
+    }
 }
