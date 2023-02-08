ee38424be768f35046811909b5244c4cbbe33476
doyeonjeong <debby_@kakao.com>
2023-02-08 16:28:17 +0900

    ✨ 5.4.1 UserInfoManager 생성 & UserInfoKey 구조체 생성

---
+++ MyMemory/MyMemory/UserInfoManager.swift
@@ -0,0 +1,21 @@
+//
+//  UserInfoManager.swift
+//  MyMemory
+//
+//  Created by Doyeon on 2023/02/08.
+//
+
+import UIKit
+
+/// UserDefault 객체에 데이터를 저장할 때 사용될 키 값
+struct UserInfoKey {
+    static let loginId = "LOGINID"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = "PROFILE"
+}
+
+/// 계정 및 사용자 정보를 저장 관리하는 클래스
+class UserInfoManager {
+    
+}
