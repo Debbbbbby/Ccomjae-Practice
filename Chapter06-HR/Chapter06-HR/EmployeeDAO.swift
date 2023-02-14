51342af75ac3a1cbc0b1ecc91aaf0e14a3dab5df
doyeonjeong <debby_@kakao.com>
2023-02-14 22:04:26 +0900

    ✨ 단일 사원 정보 불러오기

--- Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
+++ Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
@@ -84,4 +84,34 @@ class EmployeeDAO {
         }
         return employeeList
     }
+    
+    /// 단일 사원 정보 불러오기
+    func getOne(empCd: Int) -> EmployeeVO? {
+        let sql = """
+            SELECT emp_cd, emp_name, join_date, state_cd, department.depart_title
+              FROM employee
+              JOIN department On department.depart_cd = employee.depart_cd
+             WHERE emp_cd = ?
+        """
+        
+        let rs = self.fmdb.executeQuery(sql, withArgumentsIn: [empCd])
+        
+        if let _rs = rs { // 결과가 옵셔널 타입이기 때문에 옵셔널 바인딩 필요
+            _rs.next()
+            
+            var record = EmployeeVO()
+            
+            record.empCd = Int(_rs.int(forColumn: "emp_cd"))
+            record.empName = _rs.string(forColumn: "emp_name")!
+            record.joinDate = _rs.string(forColumn: "join_date")!
+            record.departTitle = _rs.string(forColumn: "depart_title")!
+            
+            let cd = Int(_rs.int(forColumn: "state_cd"))
+            record.stateCd = EmpStateType(rawValue: cd)!
+            
+            return record
+        } else {
+            return nil
+        }
+    }
 }
