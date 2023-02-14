84c8f771e19ff00e643922d78c2029274d0d82e5
doyeonjeong <debby_@kakao.com>
2023-02-14 20:24:54 +0900

    ✨ 기본 스토리보드 구성 & 뷰 컨트롤러 연결

---
+++ Chapter06-HR/Chapter06-HR/DepartmentListVC.swift
@@ -0,0 +1,12 @@
+//
+//  DepartmentListVC.swift
+//  Chapter06-HR
+//
+//  Created by Doyeon on 2023/02/14.
+//
+
+import UIKit
+
+class DepartmentListVC: UITableViewController {
+    
+}
---
+++ Chapter06-HR/Chapter06-HR/EmployeeListVC.swift
@@ -0,0 +1,12 @@
+//
+//  EmployeeListVC.swift
+//  Chapter06-HR
+//
+//  Created by Doyeon on 2023/02/14.
+//
+
+import UIKit
+
+class EmployeeListVC: UITableViewController {
+
+}
