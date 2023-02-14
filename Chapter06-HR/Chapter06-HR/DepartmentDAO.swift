cc39e978b9360bf3c4ae8944adc913ad33b32c2a
doyeonjeong <debby_@kakao.com>
2023-02-14 20:55:00 +0900

    ✨ SQLite 연결 및 초기화 프로퍼티

---
+++ Chapter06-HR/Chapter06-HR/DepartmentDAO.swift
@@ -0,0 +1,32 @@
+//
+//  DepartmentDAO.swift
+//  Chapter06-HR
+//
+//  Created by Doyeon on 2023/02/14.
+//
+
+class DepartmentDAO {
+    
+    // 부서 정보를 담을 튜플 타입 정의
+    typealias DepartRecord = (Int, String, String)
+    
+    // SQLite 연결 및 초기화
+    lazy var fmdb = FMDatabase! = {
+        // 1. 파일 매니저 객체 생성
+        let fileMgr = FileManager.default
+        
+        // 2. 샌드박스 내 문서 디렉토리에서 DB파일 경로 확인
+        let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
+        
+        // 3. 샌드박스 경로에 파일이 없다면 메인 번들에 만들어 둔 hr.sqlite를 가져와 복사
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        
+        // 4. 준비된 DB 파일을 바탕으로 FMDatabase 객체 생성
+        let db = FMDatabase(path: dbPath)
+        return db
+    }
+}
