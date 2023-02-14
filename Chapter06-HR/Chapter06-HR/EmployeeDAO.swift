b65d78c391b11dfc901390de1fe8067f43752c85
doyeonjeong <debby_@kakao.com>
2023-02-14 21:52:32 +0900

    ✨ EmployeeDAO : DB 설정

--- Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
+++ Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
@@ -32,4 +32,23 @@ struct EmployeeVO {
 
 class EmployeeDAO {
     
+    lazy var fmdb: FMDatabase! = {
+        let fileMgr = FileManager.default
+        let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        let db = FMDatabase(path: dbPath)
+        return db
+    }()
+    
+    init() {
+        self.fmdb.open()
+    }
+    
+    deinit {
+        self.fmdb.close()
+    }
 }
