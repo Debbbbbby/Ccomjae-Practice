4fd2286b98f562df21344e3a411d0a8ed91f3260
doyeonjeong <debby_@kakao.com>
2023-02-14 20:06:58 +0900

    🎉 6.6.2 Chapter06-HR project init

---
+++ Chapter06-HR/Chapter06-HR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by Doyeon on 2023/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
