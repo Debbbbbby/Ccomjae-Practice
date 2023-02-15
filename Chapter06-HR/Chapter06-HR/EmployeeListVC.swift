a0354e77f9ef09ad5847ff59867d91fb0f1760a3
doyeonjeong <debby_@kakao.com>
2023-02-15 11:58:02 +0900

    ✨사원 목록 : 기본화면 및 목록 구현

--- Chapter06-HR/Chapter06-HR/EmployeeListVC.swift
+++ Chapter06-HR/Chapter06-HR/EmployeeListVC.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class EmployeeListVC: UITableViewController {
     
+    // MARK: - Properties
+    var empList: [EmployeeVO]! // 데이터 소스용 멤버 변수
+    var empDAO = EmployeeDAO() // SQL 처리 담당 DAO 객체
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
-        super.viewDidLoad()
+        self.empList = self.empDAO.findAll() // 기존 정보 불러오기
+        self.initUI()
+    }
+    
+    /// UI초기화
+    func initUI() {
+        
+        // 1. 내비게이션 타이틀용 레이블 속성 설정
+        let navTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        navTitle.numberOfLines = 2
+        navTitle.textAlignment = .center
+        navTitle.font = UIFont.systemFont(ofSize: 18)
+        navTitle.text = "사원 목록 \n" + " 총 \(self.empList.count) 개"
+        
+        // 2. 내비게이션 바 UI 설정
+        self.navigationItem.titleView = navTitle
+        self.navigationItem.leftBarButtonItem = self.editButtonItem // 편집 버튼 추가
+        
+        // 3. 셀을 스와이프했을 때 편집 모드가 되도록 설정
+        self.tableView.allowsSelectionDuringEditing = true
+        
         // 테이블 뷰 하단에 뷰를 넣어 빈 셀이 표시되는 것 방지
         let dummyView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         self.tableView.tableFooterView = dummyView
     }
+    
+    // MARK: - Methods
+    // MARK: Table View Delegate
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.empList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rowData = self.empList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EMP_CELL")
+        
+        cell?.textLabel?.text = rowData.empName + "(\(rowData.stateCd.desc()))"
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        cell?.detailTextLabel?.text = rowData.departTitle
+        cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        return cell!
+    }
+    
+    // MARK: @IBAction
     
 }
