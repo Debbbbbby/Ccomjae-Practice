f5fbbe3df397a3f990287e91f2cfc44243cff852
doyeonjeong <debby_@kakao.com>
2023-02-15 16:55:51 +0900

    ✨ EmployeeDAO : 재직 상태 변경 메서드 추가

--- Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
+++ Chapter06-HR/Chapter06-HR/EmployeeDAO.swift
@@ -153,4 +153,20 @@ class EmployeeDAO {
         }
     }
     
+    /// 재직 상태 변경
+    func editState(empCd: Int, stateCd: EmpStateType) -> Bool {
+        do {
+            let sql = " UPDATE Employee SET state_cd = ? WHERE emp_cd = ? "
+            
+            var params = [Any]()
+            params.append(stateCd.rawValue)
+            params.append(empCd)
+            
+            try self.fmdb.executeUpdate(sql, values: params)
+            return true
+        } catch let error as NSError {
+            print("UPDATE Error : \(error.localizedDescription)")
+            return false
+        }
+    }
 }
