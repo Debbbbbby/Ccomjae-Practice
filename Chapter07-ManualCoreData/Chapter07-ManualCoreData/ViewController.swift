6273ea33c270793f993380eb006a796027be0366
doyeonjeong <debby_@kakao.com>
2023-02-22 16:24:36 +0900

    🎉  7.5.1 코어데이터 수동 반입하기 프로젝트 생성

---
+++ Chapter07-ManualCoreData/Chapter07-ManualCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter07-ManualCoreData
+//
+//  Created by Doyeon on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
