894b06196fdd4cfd432f1cfb7a9636f031bd3efc
doyeonjeong <debby_@kakao.com>
2023-02-22 13:38:38 +0900

    ✨ 7.3.3 목록 구현하기

--- Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
+++ Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
@@ -11,23 +11,48 @@ import CoreData
 class ListVC: UITableViewController {
     
     // MARK: - Properties
-    
+    // 데이터 소스 역할을 할 배열 변수
+    lazy var list: [NSManagedObject] = {
+        return self.fetch()
+    }()
+
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    // MARK: - Methods
+    // 데이터을 읽어올 메서드
+    func fetch() -> [NSManagedObject] {
+        // 1. 앱 델리게이트 객체 참조
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate // WHERE
+        // 2. 관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext // WHAT
+        // 3. 요청 객체 생성
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Board") // HOW, 유사 SELECT
+        // 4. 데이터 가져오기
+        let result = try! context.fetch(fetchRequest)
+        return result
     }
     
-    // MARK: - Methods
+    // MARK: Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tv: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 해당하는 데이터 가져오기
+        let record = self.list[indexPath.row]
+        let title = record.value(forKey: "title") as? String
+        let contents = record.value(forKey: "contents") as? String
+        
+        // 셀 생성 및 대입
+        let cell = tv.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = contents
+        
+        return cell
+    }
+    
 }
