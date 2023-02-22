99ed975f8d19f1b226b77b7ba6cbb8884def7972
doyeonjeong <debby_@kakao.com>
2023-02-22 16:01:26 +0900

    ✨7.4.5 게시물별 로그 내역 화면 구현

--- Chapter07-CoreData/Chapter07-CoreData/LogVC.swift
+++ Chapter07-CoreData/Chapter07-CoreData/LogVC.swift
@@ -9,7 +9,27 @@ import UIKit
 import CoreData
 
 class LogVC: UITableViewController {
+    var board: BoardMO! // 게시글 정보를 전달받을 변수
     
+    lazy var list: [LogMO]! = {
+        return self.board.logs?.array as! [LogMO]
+    }()
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = self.board.title
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "logcell")!
+        cell.textLabel?.text = "\(row.regdate!)에 \(row.type.toLogType()) 되었습니다."
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        return cell
+    }
 }
 
 public enum LogType: Int16 {
