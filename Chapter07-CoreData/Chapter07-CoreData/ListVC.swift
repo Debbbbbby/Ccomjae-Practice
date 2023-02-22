8a8143b72a9b1cc9b797971b9a919219c11abf92
doyeonjeong <debby_@kakao.com>
2023-02-22 13:22:15 +0900

    ✨ Add Core Data & Set Base

---
+++ Chapter07-CoreData/Chapter07-CoreData/ListVC.swift
@@ -0,0 +1,33 @@
+//
+//  ListVC.swift
+//  Chapter07-CoreData
+//
+//  Created by Doyeon on 2023/02/22.
+//
+
+import UIKit
+import CoreData
+
+class ListVC: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    // MARK: - Methods
+}
