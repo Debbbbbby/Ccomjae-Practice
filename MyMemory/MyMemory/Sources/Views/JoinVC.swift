e9c33f7d321137bb26b97f3e4f445651b718fc45
doyeonjeong <debby_@kakao.com>
2023-02-23 17:21:18 +0900

    ✨8.4.2 계정 등록 화면 & 프로필 이미지 설정

---
+++ MyMemory/MyMemory/Sources/Views/JoinVC.swift
@@ -0,0 +1,115 @@
+//
+//  JoinVC.swift
+//  MyMemory
+//
+//  Created by Doyeon on 2023/02/23.
+//
+
+import UIKit
+import Alamofire
+import Photos
+
+class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 테이블 뷰에 들어갈 텍스트 필드들
+    var fieldAccount: UITextField!
+    var fieldPassword: UITextField!
+    var fieldName: UITextField!
+    
+    override func viewDidLoad() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        // 프로필 이미지 둥글게 처리
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+        self.profileImageView.layer.masksToBounds = true
+        
+        // 프로필 이미지에 탭 제스처 및 액션 이벤트 설정
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tappedProfile))
+        self.profileImageView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func tappedProfile(_ sender: Any) {
+        // 전반부) 원하는 소스 타입을 선택할 수 있는 액션 시트 구현
+        let msg = "프로필 이미지를 읽어올 곳을 선택하세요."
+        let sheet = UIAlertController(title: msg, message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "취소", style: . cancel))
+        sheet.addAction(UIAlertAction(title: "저장된 앨범", style: . default) { (_) in
+            selectLibrary(src: .savedPhotosAlbum)
+        })
+        sheet.addAction(UIAlertAction(title: "포토 라이브러리", style: . default) { (_) in
+            selectLibrary(src: .photoLibrary)
+        })
+        sheet.addAction(UIAlertAction(title: "카메라", style: . default) { (_) in
+            selectLibrary(src: .camera)
+        })
+        
+        // 후반부) 전달된 소스 타입에 맞게 이미지 피커 창을 여는 내부 함수
+        func selectLibrary(src: UIImagePickerController.SourceType) {
+            if UIImagePickerController.isSourceTypeAvailable(src) {
+                let picker = UIImagePickerController()
+                picker.delegate = self
+                picker.allowsEditing = true
+                
+                self.present(picker, animated: false)
+            } else {
+                self.alert("사용할 수 없는 타입입니다.")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let rawVal = UIImagePickerController.InfoKey.originalImage.rawValue
+        if let img = info[UIImagePickerController.InfoKey(rawValue: rawVal)] as? UIImage {
+            self.profileImageView.image = img
+        }
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func submit(_ sender: Any) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        let tfFrame = CGRect(x: 20, y: 0, width: cell.bounds.width - 20, height: 38)
+        switch indexPath.row {
+        case 0:
+            self.fieldAccount = UITextField(frame: tfFrame)
+            self.fieldAccount.placeholder = "계정(이메일)"
+            self.fieldAccount.borderStyle = .none
+            self.fieldAccount.autocapitalizationType = .none
+            self.fieldAccount.font = UIFont.systemFont(ofSize: 16)
+            cell.addSubview(self.fieldAccount)
+        case 1:
+            self.fieldPassword = UITextField(frame: tfFrame)
+            self.fieldPassword.placeholder = "비밀번호"
+            self.fieldPassword.borderStyle = .none
+            self.fieldPassword.isSecureTextEntry = true
+            self.fieldPassword.font = UIFont.systemFont(ofSize: 16)
+            cell.addSubview(self.fieldPassword)
+        case 2:
+            self.fieldName = UITextField(frame: tfFrame)
+            self.fieldName.placeholder = "이름"
+            self.fieldName.borderStyle = .none
+            self.fieldName.font = UIFont.systemFont(ofSize: 16)
+            cell.addSubview(self.fieldName)
+        default:
+            ()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 42
+    }
+    
+}
